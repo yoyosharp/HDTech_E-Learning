c15d80c118d1275fd876995412b8c549d76430f5
yoyos <yoyosharp89@gmail.com>
2024-03-07 16:51:04 +0700

    init

---
+++ E-Learning/Backend/HDTech_E_Learning/src/main/java/com/hdtech/HDTech_E_Learning/HdTechELearningApplication.java
@@ -0,0 +1,13 @@
+package com.hdtech.HDTech_E_Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdTechELearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdTechELearningApplication.class, args);
+	}
+
+}
---
+++ E-Learning/Backend/HDTech_E_Learning/src/test/java/com/hdtech/HDTech_E_Learning/HdTechELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdtech.HDTech_E_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdTechELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
